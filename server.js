86fb965664b63b3e18fcc5c2c4e0b9b5b5a547e3
leon-do <leondo123@gmail.com>
2019-07-06 10:39:58 -0400

    lint

--- server.js
+++ server.js
@@ -1,30 +1,28 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5001
-const bodyParser = require('body-parser')
-var solc = require('solc')
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 5001;
+const bodyParser = require("body-parser");
+var solc = require("solc");
 
 express()
+  .use(bodyParser.urlencoded({ extended: true }))
+  .use(bodyParser.json())
+  .use(express.static(__dirname + "/public"))
 
-.use(bodyParser.urlencoded({ extended: true }))
-.use(bodyParser.json())
-.use(express.static(__dirname + '/public'))
+  .get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + "/index.html"));
+  })
 
-.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/index.html'));
-})
+  .post("/", (req, res) => {
+    const input = req.body.input;
+    var output = solc.compile(input, 1);
+    for (var contractName in output.contracts) {
+      // code and ABI that are needed by web3
+      res.send({
+        bytecode: output.contracts[contractName].bytecode,
+        abi: JSON.parse(output.contracts[contractName].interface)
+      });
+    }
+  })
 
-.post('/', (req, res) => {
-	const input = req.body.input
-	var output = solc.compile(input, 1)
-	for (var contractName in output.contracts) {
-	    // code and ABI that are needed by web3
-	    res.send({
-	    	bytecode: output.contracts[contractName].bytecode,
-	    	abi: JSON.parse(output.contracts[contractName].interface)
-	    })
-	}
-
-})
-
-.listen(PORT, () => console.log(PORT))
+  .listen(PORT, () => console.log(PORT));
